558b48ee74109d7a0d70d87078f2e99b95b51f2f
Hartichaw <abiven.ced@gmail.com>
2021-09-25 09:54:30 +0200

    first layout

--- utopia_app.py
+++ utopia_app.py
@@ -5,6 +5,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 
 NAMES =["jean", "hubert", "louis"]
@@ -37,18 +38,44 @@ class colonScreen(GridLayout):
  	   def __init__(self, colony, **kwargs):
  	   	super(colonScreen, self).__init__(**kwargs)
  	   	for i in range(0, len(colony)-1):
- 	   		self.add_widget(Label(text=repr(colony[i])))
- 	   		
-class UtopiaGame(Widget):
+ 	   		self.add_widget(Label(text=repr(colony[i])))	   			 	
+class Cafeteria(Button):
     pass
-class UIWidget(Widget):
+class Well(Button):
     pass
-class MapWidget(Widget):
-	pass
-
+class Generator(Button):
+    pass			   			   			   		
+class Room(Button):
+        def __init__(self, **kwargs):
+            super().__init__( **kwargs)
+            self.count = 0
+        
+        def add_count(self):
+             self.count += 1
+             self.text= str(self.count)
+        		   		
+class GameArea(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__( **kwargs)
+        
+        for i in range(0, 12):
+            button = Room()
+            self.add_widget(button)
+            
+class TopBar(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__( **kwargs)
+        self.food_count = 0
+        
+    def add_food(self):
+        self.food_count += 1
+        self.food_lbl.text = "Food = " + str(self.food_count)
+ 
+class UtopiaGame(BoxLayout):
+        pass
+        
 class UtopiaApp(App):
-
-    def build(self):
+        
         colony = []
         layout = GridLayout(cols=3)
         for i in range(0, 7):
@@ -56,10 +83,8 @@ class UtopiaApp(App):
         	settler_c.randomize_infos()
         	colony.append(settler_c)
         #return colonScreen(cols=1, colony = colony)
-        for i in range(0, 12):
-    	    button = Button()
-    	    layout.add_widget(button)
-        return UtopiaGame()
+       
+        
 
 if __name__ == '__main__':
     UtopiaApp().run()
